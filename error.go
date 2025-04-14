88b02f7c650541363fbdb8063e26290f0dadc650
jeremy rodarellis <jeremy.rodarellis@gmail.com>
2025-04-14 22:53:56 +1000

    fix: logger so it works

--- error.go
+++ error.go
@@ -12,18 +12,22 @@ type handshakeError struct {
 }
 
 var logger *log.Logger
+var f *os.File
 
 func (h *handshakeError) Error() string {
 	return fmt.Sprintf("error: %v, value: %s", h.err, h.val)
 }
 
 func SetLogger() {
-	f, err := os.OpenFile("wslogdmp.txt", os.O_RDWR|os.O_APPEND, 0666)
+	f, err := os.OpenFile("wslogdmp.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
 	logger = &log.Logger{}
 	if err != nil {
 		logger.Fatalf("error opening file: %v", err)
 	}
-	defer f.Close()
 	logger.SetFlags(log.LstdFlags | log.Lshortfile)
 	logger.SetOutput(f)
+}
+
+func CloseLogger() {
+	f.Close()
 }
